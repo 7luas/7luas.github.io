2168c6c1eda89d98539e94e95a38985d3224941a
7luas <159937802+7luas@users.noreply.github.com>
2025-06-18 01:39:53 +0100

    Update script.js

--- sea/script.js
+++ sea/script.js
@@ -1084,8 +1084,8 @@ function updatePointerCanvasPos(theEvent){
 		}
 	}
 	
-	mouseX = theEvent.x*canvasRes;
-	mouseY = theEvent.y*canvasRes;
+	mouseX = theEvent.targetTouches[0].pageX.x*canvasRes;
+	mouseY = theEvent.targetTouches[0].pageY.y*canvasRes;
 }
 
 function readLocalVars(){													
